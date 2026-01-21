7964d388f8e654606e1f291cc5b23c5e1e60420f
Edward Yeh <yhh76227@gmail.com>
2026-01-22 03:04:19 +0800

    feat(design_explore): update tcl script

--- tcl/design_explore.tcl
+++ tcl/design_explore.tcl
@@ -50,7 +50,7 @@ proc user_help_des { args } {
     echo ""
 }
 
-define_proc_attributes user_help -info "Show the user procedure list" \
+define_proc_attributes user_help_des -info "Show the user procedure list" \
     -define_args { \
         {-only_group "Only show group titles"                              ""  boolean optional}
         {-group      "Only show the user procedures of the specific group" gid int     optional}
@@ -65,28 +65,21 @@ dict append USER_HELP_DES "Common Function" { explore_design_pads "Explore desig
 proc explore_design_pads { args } {
     parse_proc_arguments -args $args argsp
 
-    if {[info exists argsp(-part_filter)]} {
-        set PART_FILTER "^${argsp(-part_filter)}$"
-    } else {
-        set PART_FILTER "^(chip_top\w+)/.+$"
-    }
+    set PART_REGEXP "^(chip_top\\w+)/.+$"
 
-    if {[info exists argsp(-pad_filter)]} {
-        set PAD_FILTER $argsp(-pad_filter)
-    } elseif {[info exists argsp(-top)]} {
-        set PAD_FILTER "is_hierarchical==false && full_name=~chip_top\w+/i_ip\w+/u_\w+"
+    if {[info exists argsp(-top)]} {
+        set PAD_FILTER "is_hierarchical==false && full_name=~chip_top*/i_ip*/u_*"
     } else {
-        set PAD_FILTER "is_hierarchical==false && full_name=~i_ip\w+/u_\w+"
+        set PAD_FILTER "is_hierarchical==false && full_name=~i_ip*/u_*"
     }
 
     set PART_PAD_DICT [dict create]
     foreach_in_col port [get_ports * -quiet -filter "direction==inout"] {
-        set pad_cell [filter_col [get_cells -of [get_pins -of [get_nets -seg -of $port]]] \
-                                 -regexp "$PAD_FILTER"]
+        set pad_cell [get_cells -of [get_pins -of [get_nets -seg -of $port]] -filter $PAD_FILTER] 
 
         if {[sizeof_col $pad_cell] > 0} {
             if {[info exists argsp(-top)]} {
-                regexp "$PART_FILTER" [get_object_name $pad_cell] -> part_name
+                regexp $PART_REGEXP [get_object_name $pad_cell] -> part_name
             } else {
                 set part_name "(none)"
             }
@@ -112,16 +105,15 @@ proc explore_design_pads { args } {
             }
             echo "\n"
         }
+    } else {
+        return $PART_PAD_DICT
     }
-    return $PART_PAD_DICT
 }
 
 define_proc_attributes explore_design_pads -info "Explore design PADs" \
     -define_args { \
-        {-top         "Explore design from top"                          ""         boolean optional}
-        {-part_filter "Custom filter to get the partition cell (regexp)" expression string  optional}
-        {-pad_filter  "Custom filter to get the PAD cell       (regexp)" expression string  optional}
-        {-print       "Show the result"                                  ""         boolean optional}
+        {-top   "Explore design from top" "" boolean optional}
+        {-print "Show the result"         "" boolean optional}
     }
 #}}}
 
@@ -131,80 +123,68 @@ dict append USER_HELP_DES "Common Function" { explore_design_module "Explore des
 proc explore_design_module { args } {
     parse_proc_arguments -args $args argsp
 
-    if {[info exists argsp(-part_filter)]} {
-        set PART_FILTER "^${argsp(-part_filter)}$"
-    } else {
-        set PART_FILTER "^chip_top\w+$"
-    }
+    set PART_REGEXP "^(chip_top\\w+)/.+$"
+    set PART_FILTER "chip_top*"
+    set MOD_FILTER  "chip_top*/i_chip_core*/i_core*/i_*"
 
     # format: {<regexp pattern> ... }
     if {[info exists argsp(-waive)]} {
-        set WAIVE_LIST $argsp(-waive)
+        set waive_list $argsp(-waive)
     } else {
-        set WAIVE_LIST []
+        set waive_list []
     }
 
-    if {[info exists argsp(-top)]} {
-        if {[info exists argsp(-part_filter)]} {
-            set part_coll [filter_col [get_cells * -hier] -regexp $PART_FILTER]
-        } else {
-            set part_coll [get_cells chip_top*]
-        }
-        set PART_LIST [lsort [get_object_name $part_coll]]
-    } else {
-        set PART_LIST {"(none)"}
-    }
-
-    if {[info exists argsp(-mod_filter)]} {
-        set mod_coll [filter_col [get_cells * -hier] -regexp "${argsp(-mod_filter)}"]
-    } else {
-        set mod_coll [get_cells -quiet chip_top*/i_chip_core*/i_core*/i_*]
-    }
+    set mod_coll [get_cells -quiet $MOD_FILTER]
 
     set PART_MOD_DICT [dict create]
-    foreach_in_col mod_obj $mod_coll {
-        set mod_name [get_object_name $mod_obj]
+    foreach_in_col mod_cell $mod_coll {
+        set mod_name [get_object_name $mod_cell]
 
-        set hit 0
-        foreach waive_pat $WAIVE_LIST {
+        set is_waive 0
+        foreach waive_pat $waive_list {
             if {[regexp "$waive_pat" $mod_name]} {
-                set hit 1
+                set is_waive 1
                 break
             }
         }
 
-        if {$hit} {
+        if {!$is_waive} {
             if {[info exists argsp(-top)]} {
-                regexp "$PART_FILTER" [get_object_name $mod_obj] -> part_name
+                regexp "$PART_REGEXP" $mod_name -> part_name
             } else {
                 set part_name "(none)"
             }
-            dict lappend $PART_MOD_DICT $part_name $mod_obj
+
+            if {[dict exists $PART_MOD_DICT $part_name]} {
+                set tmp_coll [dict get $PART_MOD_DICT $part_name]
+                dict set PART_MOD_DICT $part_name [add_to_col $tmp_coll $mod_cell]
+            } else {
+                dict set PART_MOD_DICT $part_name $mod_cell
+            }
         }
     }
 
     if {[info exists argsp(-print)]} {
         echo ""
-        dict for {part_name mod_list} $PART_MOD_DICT {
+        dict for {part_name mod_coll} $PART_MOD_DICT {
             echo "====== $part_name"
             echo [format "%-60s %-20s" "Instance" "Design"]
             echo [string repeat "=" [expr 60 + 20]]
-            foreach mod_obj $mod_list {
-                echo [format "%-60s %-20s" [get_object_name $mod_obj] [get_attr $mod_obj ref_name]]
+            foreach_in_col mod_cell $mod_coll {
+                echo [format "%-60s %-20s" [get_object_name $mod_cell] [get_attr $mod_cell ref_name]]
             }
             echo "\n"
         }
+    } else {
+        return $PART_MOD_DICT
     }
-    return $PART_MOD_DICT
 }
 
 define_proc_attributes explore_design_module -info "Explore design module" \
     -define_args { \
-        {-top         "Explore from top"                                 ""         boolean optional}
-        {-part_filter "Custom filter to get the partition cell (regexp)" expression string  optional}
-        {-mod_filter  "Custom filter to get the design module (regexp)"  expression string  optional}
-        {-waive       "Waive constraint (regexp)"                        list       list    optional}
-        {-print       "Show the result"                                  ""         boolean optional}
+        {-top   "Explore from top"          ""   boolean optional}
+        {-waive "Waive constraint (regexp)" list list    optional}
+        {-print "Show the result"           ""   boolean optional}
     }
 #}}}
 
@@ -214,76 +194,101 @@ dict append USER_HELP_DES "Common Function" { explore_design_io "Explore design 
 proc explore_design_io { args } {
     parse_proc_arguments -args $args argsp
 
-    if {[info exists argsp(-part_filter)]} {
-        set PART_FILTER "^${argsp(-part_filter)}$"
-    } else {
-        set PART_FILTER "^chip_top\w+$"
-    }
+    set PART_REGEXP "^(chip_top\\w+)/.+$"
+    set PART_FILTER "chip_top*"
 
-    # format: {<group_name> <regexp_pattern> ... }
+    # format: { \
+    #   group1 {tag11 regexp11 tag12 regexp12 ... } \
+    #   group2 {tag21 regexp21 tag22 regexp22 ... } \
+    #   ...
+    # }
     if {[info exists argsp(-group)]} {
-        set GROUP_DICT $argsp(-group)
+        set group_dict $argsp(-group)
     } else {
-        set GROUP_DICT {}
+        set group_dict {}
     }
 
-    set WAIVE_COLL {}
+    set waive_coll {}
     if {[info exists argsp(-top)]} {
         if {[info exists argsp(-waive)]} {
-            set WAIVE_COLL [get_pins -quiet $argsp(-waive)]
+            set waive_coll [get_pins -quiet $argsp(-waive)]
         }
 
         set io_dict [dict create]
-        foreach_in_col part_cell [filter [get_cells * -hier] -regexp $PART_FILTER] {
-            set pin_coll [remove_from_col [get_pins -of $part_cell] $WAIVE_COLL]
+        foreach_in_col part_cell [get_cells $PART_FILTER] {
+            set pin_coll [remove_from_col [get_pins -of $part_cell] $waive_coll]
             dict set io_dict [get_object_name $part_cell] $pin_coll 
         }
     } else {
         if {[info exists argsp(-waive)]} {
-            set WAIVE_COLL [get_ports -quiet $argsp(-waive)]
+            set waive_coll [get_ports -quiet $argsp(-waive)]
         }
-        set port_coll [remove_from_col [get_ports * -filter "direction!=inout"] $WAIVE_COLL]
-        set io_dict [dict create "(none)" $port_coll]
+        set port_coll [remove_from_col [get_ports * -filter "direction!=inout"] $waive_coll]
+        set io_dict   [dict create "(none)" $port_coll]
     }
 
     set IO_GROUP_DICT [dict create]
+
     dict for {part_name io_coll} $io_dict {
-        set part_dict [dict create]
+        set part_dict [dict create "(none)" {}]
+
         foreach io_name [get_object_name $io_coll] {
             set gname "(none)"
-            dict for {gname_tmp pat} $GROUP_DICT {
-                if {[regexp "$pat" $io_name]} {
-                    set gname $gname_tmp
+            dict for {gname_tmp tag_dict} $group_dict {
+                dict for {tname pat} $tag_dict {
+                    if {[regexp $pat $io_name]} {
+                        set gname $gname_tmp
+                        dict lappend part_dict $gname $tname $io_name
+                        break
+                    }
+                }
+
+                if {$gname != "(none)"} {
                     break
                 }
             }
-            dict lappend part_dict $gname $io_name
+
+            if {$gname == "(none)"} {
+                dict lappend part_dict "(none)" $io_name
+            }
         }
+
         dict set IO_GROUP_DICT $part_name $part_dict
     }
 
     if {[info exists argsp(-print)]} {
         echo ""
         dict for {part_name part_dict} $IO_GROUP_DICT {
-            echo "====== $part_name"
-            echo [format "%-60s" "PinGroup"]
-            echo [string repeat "=" [expr 60]]
-            dict for {gname io_list} $part_dict {
-                echo [format "%-60s" $gname]
+            echo "====== IOGroup: $part_name"
+            echo ""
+            foreach gname [dict key $part_dict] {
+                if {$gname != "(none)"} {
+                    echo "### $gname"
+                    foreach tname [dict key [dict get $part_dict $gname]] {
+                        echo $tname
+                    }
+                    echo ""
+                } else {
+                    echo "### (none)"
+                    foreach io_name [dict get $part_dict "(none)"] {
+                        echo $io_name
+                    }
+                    echo ""
+                }
             }
-            echo "\n"
+            echo ""
         }
+    } else {
+        return $IO_GROUP_DICT
     }
-    return $IO_GROUP_DICT
 }
 
 define_proc_attributes explore_design_io -info "Explore design IO" \
     -define_args { \
-        {-top         "Explore from top"                                 ""         boolean optional}
-        {-part_filter "Custom filter to get the partition cell (regexp)" expression string  optional}
-        {-waive       "Waive pin/port list"                              waive_list list    optional}
-        {-group       "Group constraint (regexp)"                        group_dict list    optional}
-        {-print       "Show the result"                                  ""         boolean optional}
+        {-top   "Explore from top"          ""         boolean optional}
+        {-waive "Waive pin/port list"       waive_list list    optional}
+        {-group "Group constraint (regexp)" group_dict list    optional}
+        {-print "Show the result"           ""         boolean optional}
     }
 #}}}
 
